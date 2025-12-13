a8de77f208d0f66b4e71949c64126ab82438e4a9
Lukas Morgenstern <lukas.morgenstern@icloud.com>
2025-12-13 12:47:32 +0100

    Removed repetitive teacher js code Added new helper functions to student-database.js Teacher js code now uses helper functions from student-database.js

--- src/main/resources/js/admin/build_teacher.js
+++ src/main/resources/js/admin/build_teacher.js
@@ -1,36 +1,13 @@
-async function onClassChange(event) {
-    populateStudentTable(Number(event.target.value), "studentTable", (row, student) => {
-        row.innerHTML = `
-            <td class="student-name">${student.name}</td>
-            <td class="student-room">${student.room}</td>
-            <td class="student-graduation-level">${graduationLevels[student.graduationLevel]}</td>
-            <td class="student-action"><button onclick="viewStudent(${student.id})">Bearbeiten</button></td>
-        `;
-    });
-}
+let teacher = JSON.parse(sessionStorage.getItem("currentTeacher"));
+console.log(teacher);
+document.getElementById("teacher-name").textContent = teacher.firstName + " " + teacher.lastName;
+let teacherId = teacher.id;
+Array.from(document.getElementsByClassName("teacherId")).forEach(e => e.value = teacherId);
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const classSelect = document.getElementById('classSelect');
-  const subjectClassSelect = document.getElementById('classSelectSubject');
-  const classes = await fetchTeacherClasses(teacherId);
-  populateClassSelect(classSelect, classes);
-  populateClassSelect(subjectClassSelect, classes);
-  classSelect.addEventListener('change', onClassChange);
-  subjectClassSelect.addEventListener('change', (_) => populateSubjectStudentList('subjectSelect', 'classSelectSubject', 'subjectStudentTable'));
-  onClassChange({ target: classSelect }); // Trigger initial load
-
-  const subjectSelect = document.getElementById('subjectSelect');
-  populateSubjectSelect('subjectSelect', teacherId);
-  subjectSelect.addEventListener('change', (_) => populateSubjectStudentList('subjectSelect', 'classSelectSubject', 'subjectStudentTable'));
-  populateSubjectStudentList({ target: subjectSelect }); // Trigger initial load
-
-  populateSubjectSelect('editSubjectSelect');
-
-  const editClassSelect = document.getElementById('editClassSelect');
-  const allClasses = await fetchClasses();
-  populateClassSelect(editClassSelect, allClasses);
-
-  populateRoomSelect('roomSelect');
-  roomSelect.addEventListener('change', (e) => populateRoomStudentList(e.target.value));
-  populateRoomStudentList({target: roomSelect});
+    buildTeacherDashboard(fetchTeacherClasses(teacherId), fetchSubjects(teacherId));
+    populateSubjectSelect('editSubjectSelect', await fetchAllSubjects());
+    const editClassSelect = document.getElementById('editClassSelect');
+    const allClasses = await fetchClasses();
+    populateClassSelect(editClassSelect, allClasses);
 });
