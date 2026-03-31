12fca759e5632449374cacd95942e8f4e0f3ced8
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-03-31 02:00:35 +0200

    Added new navigation type for other admin pages

--- src/main/java/de/igslandstuhl/database/client/navigation/NavigationType.java
+++ src/main/java/de/igslandstuhl/database/client/navigation/NavigationType.java
@@ -2,6 +2,7 @@ package de.igslandstuhl.database.client.navigation;
 
 public enum NavigationType {
     ADMIN_DASHBOARD,
+    ADMIN_OTHER,
     ADMIN_STUDENT,
     ADMIN_STUDENT_OTHER,
     STUDENT_DASHBOARD,
