8749971011c174ba540f75b71ffeffaee0213a31
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-06-18 13:39:50 +0200

    Moved plugin loader into builtin plugin

--- src/main/java/de/igslandstuhl/database/plugins/BuiltinPlugin.java
+++
@@ -1,8 +0,0 @@
-package de.igslandstuhl.database.plugins;
-
-public abstract class BuiltinPlugin extends Plugin {
-    protected BuiltinPlugin(PluginDescription description) {
-        super();
-        init(description);
-    }
-}
--- src/main/java/de/igslandstuhl/database/plugins/PluginDescription.java
+++
@@ -1,7 +0,0 @@
-package de.igslandstuhl.database.plugins;
-
-import java.util.List;
-
-public record PluginDescription(String id, String name, String description, String main, List<String> depends) {
-    
-}
