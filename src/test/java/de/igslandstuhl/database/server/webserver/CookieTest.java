dc945cb6d2fcb8e662aafbc54599ab8c4a6160d0
synchronierer <153615360+synchronierer@users.noreply.github.com>
2026-07-28 23:26:33 +0200

    Refactor cookie parsing into Cookie

--- src/test/java/de/igslandstuhl/database/server/webserver/CookieTest.java
+++ src/test/java/de/igslandstuhl/database/server/webserver/CookieTest.java
@@ -1,5 +1,6 @@
 package de.igslandstuhl.database.server.webserver;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,5 +30,60 @@ public class CookieTest {
     @Test
     void testEquals() {
         assertEquals(cookie, new Cookie("test-key", "test-value"));
+    }
+
+    @Test
+    void testParseSingleCookie() {
+        assertArrayEquals(
+            new Cookie[] {new Cookie("test-key", "test-value")},
+            Cookie.parse("test-key=test-value")
+        );
+    }
+
+    @Test
+    void testParseMultipleCookies() {
+        assertArrayEquals(
+            new Cookie[] {
+                new Cookie("first", "one"),
+                new Cookie("second", "two")
+            },
+            Cookie.parse("first=one; second=two")
+        );
+    }
+
+    @Test
+    void testParseValueContainingEqualsSign() {
+        assertArrayEquals(
+            new Cookie[] {new Cookie("token", "part1=part2=part3")},
+            Cookie.parse("token=part1=part2=part3")
+        );
+    }
+
+    @Test
+    void testParseEmptyValue() {
+        assertArrayEquals(
+            new Cookie[] {new Cookie("empty", "")},
+            Cookie.parse("empty=")
+        );
+    }
+
+    @Test
+    void testParseIgnoresInvalidPairs() {
+        assertArrayEquals(
+            new Cookie[] {
+                new Cookie("valid", "value"),
+                new Cookie("other", "cookie")
+            },
+            Cookie.parse(
+                "invalid; =missing-name; valid=value; other=cookie"
+            )
+        );
+    }
+
+    @Test
+    void testParseEmptyHeader() {
+        assertArrayEquals(new Cookie[0], Cookie.parse(null));
+        assertArrayEquals(new Cookie[0], Cookie.parse(""));
+        assertArrayEquals(new Cookie[0], Cookie.parse("   "));
     }
 }
