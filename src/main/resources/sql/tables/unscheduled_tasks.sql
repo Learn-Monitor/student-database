118e7b441e9a3536f3bd7fcb81dd033b3133d6f8
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-08-03 00:35:20 +0200

    Fixed unscheduled_tasks.sql

--- src/main/resources/sql/tables/unscheduled_tasks.sql
+++ src/main/resources/sql/tables/unscheduled_tasks.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS unscheduled_tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
     class INTEGER NOT NULL,
     subject INTEGER NOT NULL,
-    max_tokens INTEGER NOT NULL
+    max_tokens INTEGER NOT NULL,
 
     FOREIGN KEY (subject) REFERENCES subjects(id) ON DELETE CASCADE,
     FOREIGN KEY (class) REFERENCES classes(id) ON DELETE CASCADE,
