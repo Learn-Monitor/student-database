c302859efffb6ed6cd9f733f45a33e415ee3b635
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-08-02 18:55:00 +0200

    Tests now compile again

--- src/test/java/de/igslandstuhl/database/api/PreConditions.java
+++ src/test/java/de/igslandstuhl/database/api/PreConditions.java
@@ -16,7 +16,7 @@ public class PreConditions {
         Subject.addSubject("Mathematik");
     }
     public static void addSampleTopic() throws SQLException {
-        Topic.addTopic("Bruchrechnung", Subject.get(1), 100, 5, 1);
+        Topic.addTopic("Bruchrechnung", Subject.get(1), 100, 5, 1, SchoolYear.getCurrentYear().getCurrentSemester());
     }
     public static void addSampleTask() throws SQLException {
         Task.addTask(Topic.get(1), "Addition", TaskLevel.LEVEL1);
