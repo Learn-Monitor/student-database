994584dfa22ddd2162460df18c8699a76ae44836
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-08-03 00:00:32 +0200

    Fixed little bug

--- src/main/resources/sql/pushes/add_topic.sql
+++ src/main/resources/sql/pushes/add_topic.sql
@@ -1,4 +1,3 @@
 INSERT INTO topics (name, subject, grade, number, semester)
 VALUES (?, ?, ?, ?, ?)
-ON CONFLICT(name, subject, grade) DO UPDATE SET
-    ratio = excluded.ratio;
+ON CONFLICT(name, subject, grade) DO NOTHING;
